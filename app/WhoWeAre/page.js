7095314d077363ea9e81d64d99702e4825ea0a9f
Sean Belon <sbelon1@umbc.edu>
2025-08-21 20:43:49 -0400

    my b, put an extra space ;(

--- app/WhoWeAre/page.js
+++ app/WhoWeAre/page.js
@@ -80,7 +80,7 @@ export default function WhoWeAre() {
                 <p className="text-lg text-gray-800">We make the complex simple and understandable.</p>
               </div>
               <div className="bg-blue-200 rounded-md p-6 flex flex-col items-center text-center text-xl">
-                <div className="bg-orange-4 00 rounded-full w-20 h-20 flex items-center justify-center mb-4">
+                <div className="bg-orange-400 rounded-full w-20 h-20 flex items-center justify-center mb-4">
                   {/* Group with heart Icon */} 
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87M12 12a4 4 0 100-8 4 4 0 000 8z" />
